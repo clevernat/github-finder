2e1e283a2274f35b14531227dc6e41a81083d873
iamdevnat <iamdevnat@gmail.com>
2021-01-20 05:19:20 -0800

    change per-page to page

--- js/https.js
+++ js/https.js
@@ -1,13 +1,13 @@
 class Github{
     constructor() {
         this.client_id = '7d24ba54a01428bc9098';
-        this.per_page = 5
+        this.page = 5
     }
 
 
     async get(user) {
         const res = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}`);
-        const repos = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&per_page=${this.per_page}`);
+        const repos = await fetch(`https://api.github.com/users/${user}/repos?client_id=${this.client_id}&per_page=${this.page}`);
 
         const userData = await res.json();
         const reposUserData = await repos.json();
